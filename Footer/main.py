a3d260dd62c71094e0054e6ecb8585757049fa3b
soft0725 <a01021146076@gmail.com>
2023-10-06 23:33:11 +0900

    footer 부분 (setting, report, Health) 중 setting 완료

--- Footer/main.py
+++ Footer/main.py
@@ -64,12 +64,8 @@ class SettingPart:
 if __name__ == "__main__":
     print("main 부분 실행")
     root = Tk()  # Tk 생성
-    # root.overrideredirect(True)
     canvas = Canvas(root, bg="#1b1b1b")
-    # gui화면 설정 배경 bg="색갈입력" 현재 #1b1b1b 설정됨
     canvas.pack(fill=BOTH, expand=TRUE)
-    # footer.footer_menu(canvas, root)
     SettingPart(canvas, root).create_window()
     root.geometry("1920x1080")
-    # 화면 크기를 지정한다
     root.mainloop()
