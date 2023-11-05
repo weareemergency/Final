f3623577cb1e82a2cdbf5152eeb42d6a2db1a2ad
jiseok12 <103105145+jiseok12@users.noreply.github.com>
2023-11-05 22:40:24 +0900

    camera test7

--- camara.py
+++ camara.py
@@ -1,10 +1,19 @@
-import nanocamera as nano
+import picamera
+import time
+import cv2
 
 # 카메라 초기화
-camera = nano.Camera(camera_type=1, device_id=1, width=640, height=480, fps=30)
+camera = picamera.PiCamera()
+
+# 카메라 설정
+camera.resolution = (640, 480)
+camera.framerate = 30
 
 # 카메라에서 프레임 캡처
-frame = camera.read()
+camera.capture('/tmp/foo.jpg')
+
+# 캡처된 프레임 읽기
+frame = cv2.imread('/tmp/foo.jpg')
 
 # 캡처된 프레임 출력
 cv2.imshow('frame', frame)
@@ -14,6 +23,3 @@ cv2.waitKey(0)
 
 # 모든 윈도우 닫기
 cv2.destroyAllWindows()
-
-# 카메라 해제
-camera.release()
